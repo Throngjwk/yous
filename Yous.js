965ff15edd720386edfaf59d8bc558d26382ac74
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-06 21:37:43 -0300

    wow

--- Yous.js
+++ Yous.js
@@ -57,9 +57,20 @@ var init = () => {
     // y
     {
         let getDesc = (level) => "y=" + getY(level).toString(0);
-        y = theory.createUpgrade(2, currency, new FirstFreeCost(new ExponentialCost(1e7, Math.log2(2))));
+        y = theory.createUpgrade(3, currency, new FirstFreeCost(new ExponentialCost(1e7, Math.log2(2))));
         y.getDescription = (_) => Utils.getMath(getDesc(y.level));
         y.getInfo = (amount) => Utils.getMathTo(getDesc(y.level), getDesc(y.level + amount));
+    }
+
+    // prestige
+    {
+        prestige = theory.createUpgrade(0, currency, new FreeCost());
+        prestige.getDescription = (_) => "Open t clicker Menu";
+        prestige.getInfo = (amount) => "Open t clicker Menu";
+        prestige.boughtOrRefunded = (_) => {
+            psPUP.show();
+            prestige.level = 0;
+        }
     }
 
     // pertament
@@ -69,7 +80,7 @@ var init = () => {
 
     // milestone upg
     {
-        unlockXX = theory.createMilestoneUpgrade(0, 3);
+        unlockXX = theory.createMilestoneUpgrade(0, 1);
         unlockXX.description = "Unlock at $x_x$.";
         unlockXX.info = "Go go";
         unlockXX.boughtOrRefunded = (_) => { 
@@ -77,9 +88,103 @@ var init = () => {
             theory.invalidatePrimaryEquation()
          };
     }
+
+    {
+        xExp = theory.createMilestoneUpgrade(1, 2);
+        xExp.description = Localization.getUpgradeIncCustomExpDesc("x", "0.05");
+        xExp.info = Localization.getUpgradeIncCustomExpInfo("x", "0.05");
+        xExp.boughtOrRefunded = (_) => {
+         updateAvailability();
+         theory.invalidatePrimaryEquation();
+        }
+    }
+
+    {
+        unlockTC = theory.createMilestoneUpgrade(2, 1);
+        unlockTC.description = "Unlock at t Clicker.";
+        unlockTC.info = "Go go";
+        unlockTC.boughtOrRefunded = (_) => { 
+            updateAvailability();
+            theory.invalidatePrimaryEquation()
+         };
+    }
+
+
+    //Update Popup
+    function UpdateTClicker() {
+        popup = ui.createPopup({
+            title: "T Clicker",
+            content: ui.createStackLayout({
+                children: [
+                    ui.createLabel({
+                        text: "You Have " + t + " t.",
+                        fontSize:19,
+                        horizontalTextAlignment: TextAlignment.CENTER,
+                    }),
+                    ui.createButton({
+                        text: "Click Me!",
+                        onClicked: () => {
+                            t += tpc
+                            updateTClicker()
+                        }}),
+                    ui.createButton({text: "Close", onClicked: () => popup.hide()}),
+                ]
+            })
+        });
+    }
+
+    //UI Popup
+    var popup = ui.createPopup({
+        title: "T Clicker",
+        content: ui.createStackLayout({
+            children: [
+                ui.createLabel({
+                    text: "You Have " + t + " t.",
+                    fontSize:19,
+                    horizontalTextAlignment: TextAlignment.CENTER,
+                }),
+                ui.createButton({
+                    text: "Click Me!",
+                    onClicked: () => {
+                        t += tpc
+                        updateTClicker()
+                    }}),
+                ui.createButton({text: "Close", onClicked: () => popup.hide()}),
+            ]
+        })
+    });
 }
 
 var updateAvailability = () => {
     xExp.isAvailable = unlockXX.level > 0;
     xx.isAvailable = unlockXX.level > 0;
 }
+
+var tick = (elapsedTime, multiplier) => {
+    let dt = BigNumber.from(elapsedTime * multiplier);
+    let bonus = theory.publicationMultiplier;
+    t += tps;
+    currency.value += dt * bonus * getW(w.level) *
+                                   getX(x.level);
+}
+
+var getPrimaryEquation = () => {
+    let result = "\\dot{\\rho} = w";
+
+    result += "x";
+
+    return result;
+}
+
+var getSecondaryEquation = () => theory.latexSymbol + "=\\max\\rho";
+var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
+var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.164}}{3}";
+var getTau = () => currency.value;
+var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
+
+var getW = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
+var getX = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
+var getXX = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
+var getY = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
+
+init();
